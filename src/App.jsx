ba9c8c3b76dea8fbd3c4a771293c236f84a93c78
Kgomotso <kgomotsomkhawane2001@gmail.com>
2025-01-10 14:07:12 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ function App() {
     <UserContext.Provider value={listings}>
       <HeaderSection/>
       <Routes>
-        <Route path='/Nola-Estates' element={
+        <Route path='/Nola-Estates/' element={
           <>
             <LandingPage/>
             <InfoPage/>
